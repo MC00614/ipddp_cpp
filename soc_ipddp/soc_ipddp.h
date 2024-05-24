3e7218bba538d3ecce976c914261cfd6796bf1d7
mczb <12190588@inha.edu>
2024-05-24 20:08:51 +0200

    Need to Check Backward

--- soc_ipddp/soc_ipddp.h
+++ soc_ipddp/soc_ipddp.h
@@ -92,23 +92,6 @@ void SOC_IPDDP::solve() {
 }
 
 void SOC_IPDDP::backwardPass() {
-    // Eigen::MatrixXd fx;
-    // Eigen::MatrixXd fu;
-    // Eigen::MatrixXd fxx;
-    // Eigen::MatrixXd fuu;
-    // Eigen::MatrixXd fxu;
-
-    // Eigen::MatrixXd qx;
-    // Eigen::MatrixXd qu;
-    // Eigen::MatrixXd qxx;
-    // Eigen::MatrixXd quu;
-    // Eigen::MatrixXd qxu;
-
-    // Eigen::MatrixXd Qx;
-    // Eigen::MatrixXd Qu;
-    // Eigen::MatrixXd Qxx;
-    // Eigen::MatrixXd Quu;
-    // Eigen::MatrixXd Qxu;
     Eigen::VectorXd Vx = scalarJacobian(p, X.col(N-1));
     Eigen::MatrixXd Vxx = scalarHessian(p, X.col(N-1));
 
@@ -125,34 +108,19 @@ void SOC_IPDDP::backwardPass() {
         Eigen::MatrixXd quu = scalarHessian(q, X.col(t), U.col(t), "uu");
         Eigen::MatrixXd qxu = scalarHessian(q, X.col(t), U.col(t), "xu");
 
-        Eigen::MatrixXd Qx = qx + fx.transpose()*Vx;
-        Eigen::MatrixXd Qu = qu + fu.transpose()*Vx;
+        Eigen::VectorXd Qx = qx + fx.transpose()*Vx;
+        Eigen::VectorXd Qu = qu + fu.transpose()*Vx;
         Eigen::MatrixXd Qxx = qxx + fx.transpose()*Vxx*fx + tensdot(Vx,fxx);
         Eigen::MatrixXd Quu = quu + fu.transpose()*Vxx*fu + tensdot(Vx,fuu);
         Eigen::MatrixXd Qxu = qxu + fx.transpose()*Vxx*fu + tensdot(Vx,fxu);
 
-        std::cout << "Qxx\n" << Qxx << std::endl;
-        std::cout << "Qxu\n" << Qxu << std::endl;
-        std::cout << "Quu\n" << Quu << std::endl;
-
-        // std::cout << "fx\n" << fx << std::endl;
-        // std::cout << "fxx\n" << fxx << std::endl;
-        // std::cout << "fxu\n" << fxu << std::endl;
-
-        // std::cout<<Vx<<std::endl;
-
-
-        // std::cout << fxx << std::endl;
-        // std::cout << Qu << std::endl;
-        // std::cout << Vxx.size() << std::endl;
-        // std::cout << Vxx << std::endl;
-        // std::cout << fd.transpose().rows() << fd.transpose().cols() << std::endl;
-        // std::cout << fd.middleCols(0,dim_x).transpose().rows() << fd.middleCols(0,dim_x).transpose().cols() << std::endl;
-        // std::cout << Vx.rows() << Vx.cols() << std::endl;
-        // std::cout << qd.middleCols(0,dim_x).rows() << qd.middleCols(0,dim_x).cols() << std::endl;
-        break;
+        Vx = Qx + Qxu*Quu.inverse()*Qu;
+        Vxx = Qxx + Qxu*Quu.inverse()*Qxu.transpose();
+        
+        // Expose?
+        std::cout<<"Vx\n"<<Vx<<std::endl;
+        std::cout<<"Vxx\n"<<Vxx<<std::endl;
     }
-
 }
 
 void SOC_IPDDP::forwardPass() {}
