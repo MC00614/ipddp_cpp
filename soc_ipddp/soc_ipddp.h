54c589b96f097d26af09e75f3cde558edd873d3f
mczb <12190588@inha.edu>
2024-05-26 19:36:10 +0200

    DDP Finish

--- soc_ipddp/soc_ipddp.h
+++ soc_ipddp/soc_ipddp.h
@@ -30,10 +30,15 @@ public:
 private:
     int N;
     int max_iter;
-    int dim_x;
-    int dim_u;
     Eigen::MatrixXd X;
     Eigen::MatrixXd U;
+    int dim_x;
+    int dim_u;
+    int regulate;
+
+    
+    Eigen::MatrixXd k;
+    Eigen::MatrixXd K;
 
     // Discrete Time System
     std::function<Eigen::VectorXd(Eigen::VectorXd, Eigen::VectorXd)> f;
@@ -43,7 +48,6 @@ private:
     std::function<double(Eigen::VectorXd)> p;
     void backwardPass();
     void forwardPass();
-    void regulate();
 };
 
 
@@ -61,6 +65,11 @@ void SOC_IPDDP::init(int N, int max_iter, Eigen::MatrixXd X, Eigen::MatrixXd U) 
 
     this->dim_x = X.rows();
     this->dim_u = U.rows();
+
+    this->k.resize(this->dim_u, this->N);
+    this->K.resize(this->dim_u, this->dim_x * this->N);
+
+    this->regulate = 0;
 }
 
 
@@ -83,55 +92,81 @@ void SOC_IPDDP::solve() {
     int iter = 0;
 
     while (iter++ < this->max_iter) {
+        std::cout<< "Backward Pass" << std::endl;
         this->backwardPass();
-        break;
+        std::cout<< "Forward Pass" << std::endl;
         this->forwardPass();
-        this->regulate();
     }
     
 }
 
 void SOC_IPDDP::backwardPass() {
-    Eigen::VectorXd Vx = scalarJacobian(p, X.col(N-1));
-    Eigen::MatrixXd Vxx = scalarHessian(p, X.col(N-1));
+    bool backward_failed = true;
 
-    for (int t = N-1; t >= 0; --t) {
-        Eigen::MatrixXd fx = vectorJacobian(f, X.col(t), U.col(t), "x");
-        Eigen::MatrixXd fu = vectorJacobian(f, X.col(t), U.col(t), "u");
-        Eigen::Tensor<double, 3> fxx = vectorHessian(f, X.col(t), U.col(t), "xx");
-        Eigen::Tensor<double, 3> fuu = vectorHessian(f, X.col(t), U.col(t), "uu");
-        Eigen::Tensor<double, 3> fxu = vectorHessian(f, X.col(t), U.col(t), "xu");
+    while (backward_failed) {
+        Eigen::VectorXd Vx = scalarJacobian(p, X.col(N-1));
+        Eigen::MatrixXd Vxx = scalarHessian(p, X.col(N-1));
 
-        Eigen::VectorXd qx = scalarJacobian(q, X.col(t), U.col(t), "x");
-        Eigen::VectorXd qu = scalarJacobian(q, X.col(t), U.col(t), "u");
-        Eigen::MatrixXd qxx = scalarHessian(q, X.col(t), U.col(t), "xx");
-        Eigen::MatrixXd quu = scalarHessian(q, X.col(t), U.col(t), "uu");
-        Eigen::MatrixXd qxu = scalarHessian(q, X.col(t), U.col(t), "xu");
+        for (int t = N-1; t >= 0; --t) {
+            Eigen::MatrixXd fx = vectorJacobian(f, X.col(t), U.col(t), "x");
+            Eigen::MatrixXd fu = vectorJacobian(f, X.col(t), U.col(t), "u");
+            Eigen::Tensor<double, 3> fxx = vectorHessian(f, X.col(t), U.col(t), "xx");
+            Eigen::Tensor<double, 3> fxu = vectorHessian(f, X.col(t), U.col(t), "xu");
+            Eigen::Tensor<double, 3> fuu = vectorHessian(f, X.col(t), U.col(t), "uu");
 
-        Eigen::VectorXd Qx = qx + fx.transpose()*Vx;
-        Eigen::VectorXd Qu = qu + fu.transpose()*Vx;
-        Eigen::MatrixXd Qxx = qxx + fx.transpose()*Vxx*fx + tensdot(Vx,fxx);
-        Eigen::MatrixXd Quu = quu + fu.transpose()*Vxx*fu + tensdot(Vx,fuu);
-        Eigen::MatrixXd Qxu = qxu + fx.transpose()*Vxx*fu + tensdot(Vx,fxu);
+            Eigen::VectorXd qx = scalarJacobian(q, X.col(t), U.col(t), "x");
+            Eigen::VectorXd qu = scalarJacobian(q, X.col(t), U.col(t), "u");
+            Eigen::MatrixXd qxx = scalarHessian(q, X.col(t), U.col(t), "xx");
+            Eigen::MatrixXd qxu = scalarHessian(q, X.col(t), U.col(t), "xu");
+            Eigen::MatrixXd quu = scalarHessian(q, X.col(t), U.col(t), "uu");
 
-        Vx = Qx + Qxu*Quu.inverse()*Qu;
-        Vxx = Qxx + Qxu*Quu.inverse()*Qxu.transpose();
-        
-        // Expose?
-        std::cout<<"Vx\n"<<Vx<<std::endl;
-        std::cout<<"Vxx\n"<<Vxx<<std::endl;
+            Eigen::VectorXd Qx = qx + fx.transpose()*Vx;
+            Eigen::VectorXd Qu = qu + fu.transpose()*Vx;
+            Eigen::MatrixXd Qxx = qxx + fx.transpose()*Vxx*fx + tensdot(Vx,fxx);
+            Eigen::MatrixXd Qxu = qxu + fx.transpose()*Vxx*fu + tensdot(Vx,fxu);
+            Eigen::MatrixXd Quu = quu + fu.transpose()*Vxx*fu + tensdot(Vx,fuu);
+
+            if (regulate) {Quu += std::pow(1.5, regulate) * Eigen::MatrixXd::Identity(dim_u, dim_u);}
+            Eigen::LLT<Eigen::MatrixXd> Quu_llt(Quu);
+            if (!Quu.isApprox(Quu.transpose()) || Quu_llt.info() == Eigen::NumericalIssue) {
+                regulate += 1;
+                break;
+            } 
+            if (t == 0) {backward_failed = false;}
+
+            this->k.col(t) = -Quu.inverse()*Qu;
+            this->K.middleCols(t * this->dim_x, this->dim_x) = -Quu.inverse()*Qxu.transpose();
+            // std::cout<<"Quu\n"<<Quu<<std::endl;
+            // std::cout<<"Qu\n"<<Qu<<std::endl;
+            // std::cout<<"k\n"<<k<<std::endl;
+            // std::cout<<"K\n"<<K<<std::endl;
+
+            Vx = Qx - Qxu*Quu.inverse()*Qu;
+            Vxx = Qxx - Qxu*Quu.inverse()*Qxu.transpose();
+            // std::cout<<"Vx\n"<<Vx<<std::endl;
+            // std::cout<<"Vxx\n"<<Vxx<<std::endl;
+            // std::cout<<"Quu\n"<<Quu<<std::endl;
+            // std::cout<<"Quu.inverse()\n"<<Quu.inverse()<<std::endl;
+
+        }
     }
 }
 
-void SOC_IPDDP::forwardPass() {}
-
-void SOC_IPDDP::regulate() {}
-
-
-Eigen::MatrixXd getX() {
-
+void SOC_IPDDP::forwardPass() {
+    // std::cout<<"k\n"<<k<<std::endl;
+    // std::cout<<"K\n"<<K<<std::endl;
+    Eigen::VectorXd x = X.col(0);
+    for (int t = 0; t < N; ++t) {
+        U.col(t) = U.col(t) + k.col(t) + K.middleCols(t * this->dim_x, this->dim_x)*(x - X.col(t));
+        X.col(t) = x;
+        x = f(x, U.col(t));
+    }
 }
 
-Eigen::MatrixXd getU() {
-    
+Eigen::MatrixXd SOC_IPDDP::getX() {
+    return X;
+}
+
+Eigen::MatrixXd SOC_IPDDP::getU() {
+    return U;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,9 @@
 
 #include <cmath>
 
+#include "matplotlibcpp.h"
+namespace plt = matplotlibcpp;
+
 // Discrete Time System
 Eigen::VectorXd f(Eigen::VectorXd x0, Eigen::VectorXd u) {
     const double h = 0.05;
@@ -29,29 +32,77 @@ double p(Eigen::VectorXd x) {
 }
 
 int main() {
+    // Intitial Setting
     int N = 500;
 
     int dim_x = 2;
     int dim_u = 1;
 
-    Eigen::MatrixXd X(dim_x, N);
+    Eigen::MatrixXd X = Eigen::MatrixXd::Zero(dim_x, N);
     X(0,0) = -M_PI;
     X(1,0) = 0.0;
     X(0,N-1) = 0,0;
     X(1,N-1) = 0.0;
-    Eigen::MatrixXd U(dim_u, N);
+    Eigen::MatrixXd U = Eigen::MatrixXd::Zero(dim_u, N);
     U(0,0) = 0.0;
     
+    // Solver
     SOC_IPDDP soc_ipddp;
-    soc_ipddp.init(N, 1000, X, U);
+    soc_ipddp.init(N, 10, X, U);
     soc_ipddp.setSystemModel(f);
     soc_ipddp.setStageCost(q);
     soc_ipddp.setTerminalCost(p);
 
     soc_ipddp.solve();
 
-    // Eigen::MatrixXd X = soc_ipddp.getX();
-    // Eigen::MatrixXd U = soc_ipddp.getU();
+    // Result
+    Eigen::MatrixXd X_result = soc_ipddp.getX();
+    Eigen::MatrixXd U_result = soc_ipddp.getU();
+
+
+    // // // // // // // // // // // // // // // // // // // // // // // // // 
+    //  VISUALIZATION  // VISUALIZATION  // VISUALIZATION  // VISUALIZATION // 
+    // // // // // // // // // // // // // // // // // // // // // // // // // 
+
+    // Convert Eigen::MatrixXd to std::vector for X_result
+    std::vector<std::vector<double>> X_INIT(X_result.rows(), std::vector<double>(X_result.cols()));
+    std::vector<std::vector<double>> X_RES(X_result.rows(), std::vector<double>(X_result.cols()));
+    for (int i = 0; i < X_result.rows(); ++i) {
+        for (int j = 0; j < X_result.cols(); ++j) {
+            X_INIT[i][j] = X(i,j);
+            X_RES[i][j] = X_result(i, j);
+        }
+    }
+
+    // Convert Eigen::MatrixXd to std::vector for U_result
+    std::vector<std::vector<double>> U_INIT(U_result.rows(), std::vector<double>(U_result.cols()));
+    std::vector<std::vector<double>> U_RES(U_result.rows(), std::vector<double>(U_result.cols()));
+    for (int i = 0; i < U_result.rows(); ++i) {
+        for (int j = 0; j < U_result.cols(); ++j) {
+            U_INIT[i][j] = U(i,j);
+            U_RES[i][j] = U_result(i, j);
+        }
+    }
+
+    // Plot X_result
+    for (size_t i = 0; i < X_RES.size(); ++i) {
+        plt::subplot(dim_x + dim_u, 1, i + 1);
+        plt::plot(X_INIT[i], {{"label", "Init"}});
+        plt::plot(X_RES[i], {{"label", "Result"}});
+        plt::title("X_result Dimension " + std::to_string(i));
+        plt::legend();
+    }
+
+    // Plot U_result
+    for (size_t i = 0; i < U_RES.size(); ++i) {
+        plt::subplot(dim_x + dim_u, 1, dim_x + 1 + i);
+        plt::plot(U_INIT[i], {{"label", "Init"}});
+        plt::plot(U_RES[i], {{"label", "Result"}});
+        plt::title("U_result Dimension " + std::to_string(i));
+        plt::legend();
+    }
+
+    plt::show();
 
     return 0;
 }
